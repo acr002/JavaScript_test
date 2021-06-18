04b939cb456c413636afc4c118f85701640da3c6
acr002 <tamada@across-net.co.jp>
2021-06-18 16:35:44 +0900

    Add new files

---
+++ 01 test/02 var/beginner-html-site-styled-gh-pages/scripts/querySelector.js
@@ -0,0 +1,12 @@
+
+const para = document.querySelector('p');
+para.addEventListener('click', updateName);
+function updateName() {
+  let name = prompt('your name');
+  para.textContent = 'Player 1: ' + name;
+}
+
+
+
+
+
---
+++ 01 test/03 apply-javascript/script.js
@@ -0,0 +1,15 @@
+
+function createParagraph() {
+  let para = document.createElement('p');
+  para.textContent = 'yes!';
+  document.body.appendChild(para);
+}
+
+const buttons = document.querySelectorAll('button');
+
+for(let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener('click', createParagraph);
+}
+
+
+
